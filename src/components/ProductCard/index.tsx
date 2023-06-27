a0095ba0740fd7dd370277330c4f8214383c7110
Daniel Leal <daniel.leal@bhub.ai>
2023-06-26 23:23:42 -0300

    Extract homepage components

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -8,6 +8,46 @@ type ProductCardProps = {
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({ product, screenSize }) => {
+  const MobileListView = () => (
+    <>
+      <div className="flex-1">
+        <h3 className="text-lg text-gray-700 mb-2">{product.name}</h3>
+        {product.description && (
+          <p className="text-sm text-gray-500 mb-2 line-clamp-2">
+            {product.description}
+          </p>
+        )}
+        <p className="text-lg text-green-500">{product.price}</p>
+      </div>
+      <img
+        src={product.image_url}
+        alt={product.name}
+        className="object-cover w-1/4 h-24 mr-4"
+      />
+    </>
+  )
+
+  const CardView = () => (
+    <>
+      <div className="w-4/5 p-4 flex flex-col justify-between">
+        <div>
+          <h3 className="text-lg text-gray-700 mb-2">{product.name}</h3>
+          {product.description && (
+            <p className="text-sm text-gray-500 mb-2">{product.description}</p>
+          )}
+        </div>
+        <p className="text-lg text-green-500">{product.price}</p>
+      </div>
+      <div className="w-1/5">
+        <img
+          src={product.image_url}
+          alt={product.name}
+          className="object-cover w-full h-full"
+        />
+      </div>
+    </>
+  )
+
   return (
     <div
       className={`${
@@ -16,45 +56,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, screenSize }) => {
           : 'bg-white rounded-lg shadow-md flex'
       }`}
     >
-      {screenSize === 'sm' ? (
-        <>
-          <div className="flex-1">
-            <h3 className="text-lg text-gray-700 mb-2">{product.name}</h3>
-            {product.description && (
-              <p className="text-sm text-gray-500 mb-2 line-clamp-2">
-                {product.description}
-              </p>
-            )}
-            <p className="text-lg text-green-500">R$ {product.price}</p>
-          </div>
-          <img
-            src={product.image_url}
-            alt={product.name}
-            className="object-cover w-1/4 h-24 mr-4"
-          />
-        </>
-      ) : (
-        <>
-          <div className="w-4/5 p-4 flex flex-col justify-between">
-            <div>
-              <h3 className="text-lg text-gray-700 mb-2">{product.name}</h3>
-              {product.description && (
-                <p className="text-sm text-gray-500 mb-2">
-                  {product.description}
-                </p>
-              )}
-            </div>
-            <p className="text-lg text-green-500">R$ {product.price}</p>
-          </div>
-          <div className="w-1/5">
-            <img
-              src={product.image_url}
-              alt={product.name}
-              className="object-cover w-full h-full"
-            />
-          </div>
-        </>
-      )}
+      {screenSize === 'sm' ? <MobileListView /> : <CardView />}
     </div>
   )
 }
